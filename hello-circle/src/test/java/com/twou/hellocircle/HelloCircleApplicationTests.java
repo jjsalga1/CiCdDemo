231a67978fbab6363869b401da93582218def1e5
Jose Salgado Jr <jjsalga1@uci.edu>
2022-11-15 16:18:11 -0800

    Hello Circle 1

---
+++ hello-circle/src/test/java/com/twou/hellocircle/HelloCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twou.hellocircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
